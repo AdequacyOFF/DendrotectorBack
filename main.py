bb4ee44b0f449145033c97e22862573d5778da10
StolovAR <andrey_stolov@mail.ru>
2025-09-29 23:54:42 +0300

    ready for work

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import uvicorn
 from api import init_routers
 from starlette.middleware.cors import CORSMiddleware
+from api.logging import setup_logging
 
 def init_middlewares(app: FastAPI) -> None:
     app.add_middleware(
@@ -12,15 +13,22 @@ def init_middlewares(app: FastAPI) -> None:
         allow_headers=["*"]
     )
 
+
 def create_app() -> FastAPI:
-    app = FastAPI(root_path="/api")
+    setup_logging()
+    app = FastAPI(root_path="/api",
+                  debug=True
+                  )
     init_routers(app)
     init_middlewares(app)
     return app
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:create_app", host="0.0.0.0", port=1234, reload=True)
+    uvicorn.run("main:create_app",
+                host="0.0.0.0",
+                port=1234,
+                reload=True)
 
 
 
